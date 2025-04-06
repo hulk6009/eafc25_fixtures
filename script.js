e02d98bf74ffdcfaa4b6bd104ae5c26e6db0ab1c
Crespo <60795914+hulk6009@users.noreply.github.com>
2025-04-06 20:31:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ let secondLegData = [
     ["Sunday 6th April", "Matchday 13", "ADX FC", "", "", "Mancity Jnr"],
     ["Sunday 6th April", "Matchday 13", "OLAMIX FC", "4", "4", "dblinking"],
     ["Sunday 6th April", "Matchday 13", "Barnet FC", "", "", "Mehhh"],
-    ["Sunday 6th April", "Matchday 14", "ADX FC", "", "", "NiceFC"],
+    ["Sunday 6th April", "Matchday 14", "ADX FC", "6", "2", "NiceFC"],
     ["Sunday 6th April", "Matchday 14", "Barnet FC", "4", "5", "dblinking"],
     ["Sunday 6th April", "Matchday 14", "OLAMIX FC", "", "", "Mehhh"]
 ];
